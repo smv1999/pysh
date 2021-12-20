6a211fe867e285d6467ad482e680b6988e5acc73
Vaidhyanathan S M <vaidhyanathan.sm@gmail.com>
2021-12-20 20:59:14 +0530

    Done till rm command

--- commands_list.py
+++ commands_list.py
@@ -1,16 +1,19 @@
 commands_list = ['lf', 'ldir', 'pwd', 'cd', 'manual', 'mkdir',
-                 'calendar', 'calc', 'who', 'echo', 'rm', 'cat', 'cp', 'mv']
+                 'calendar', 'calc', 'whoami', 'echo', 'rm', 'cat', 'cp', 'mv']
 
 commands_list_manual = {
     'lf': 'lists all files in the current directory',
     'ldir': 'lists all directories in the current directory',
     'pwd': 'prints the name of the current/working directory',
-    'cd': "changes the current/working directory. \n usage: 'cd [directory]'",
-    'manual': "gives information about a command. \n usage: 'manual [command] \n For example: manual pwd'",
-    'mkdir': 'make directories - used for creating directories if they do not already exist. \n usage: mkdir [directory_name]',
-    'calendar': "displays the calendar of a given date. \n usage: 'calendar [year]' or 'calendar [year] [month]' \n For example: calendar 2021 or calendar 2021 12",
-    'calc': '',
-    'who': '',
-    'echo': '',
-    'rm': ''
+    'cd': "changes the current/working directory. \n usage: 'cd [DIRECTORY]'",
+    'manual': "gives information about a command. \n usage: 'manual [COMMAND] \n For example: manual pwd'",
+    'mkdir': 'make directories - used for creating directories if they do not already exist. \n usage: mkdir [DIRECTORY_NAME]',
+    'calendar': "displays the calendar of a given date. \n usage: 'calendar [YEAR]' or 'calendar [YEAR] [MONTH]' \n For example: 'calendar 2021' or 'calendar 2021 12'",
+    'calc': "evaluates a mathematical expression. \n usage: 'calc [EXPR]'",
+    'whoami': 'displays the username of the currently logged-in user',
+    'echo': "displays a line of text. \n usage: 'echo [TEXT]'",
+    'rm': "remove files or directories. \n usage: 'rm [DIRECTORY]' or 'rm [FILE]'",
+    'cat': "",
+    'cp': "",
+    'mv': ""
 }
--- pysh.py
+++ pysh.py
@@ -6,6 +6,7 @@ from pynput.keyboard import *
 from pyautogui import typewrite
 import calendar
 import getpass
+import shutil
 
 current_platform = platform.system()
 
@@ -23,7 +24,7 @@ def main():
     while True:
         global command
         if visit_history == False:
-            command = input("~{}$ ".format(cur_dir_path))
+            command = input("~{}$ ".format(cur_dir_path)).strip('\n')
         if command == 'exit':
             commands_history.append(command)
             break
@@ -44,7 +45,7 @@ def on_key_press(key):
     if key == Key.up:
         visit_history = True
         typewrite(commands_history[-1])
-        command = input("~{}$ ".format(cur_dir_path))
+        command = input("~{}$ ".format(cur_dir_path)).strip('\n')
         return False
 
 
@@ -99,19 +100,66 @@ def execute_commands(command):
                 command_args_opt[1]))
     elif main_command == 'calendar':
         if len(command_args_opt) == 3:
-            print(calendar.month(
-                int(command_args_opt[1]), int(command_args_opt[2])))
+            try:
+                print(calendar.month(
+                    int(command_args_opt[1]), int(command_args_opt[2])))
+            except:
+                print(
+                    "pysh: calendar: invalid operand(s). \n Try 'manual calendar' for more help.")
         elif len(command_args_opt) == 2:
-            print(calendar.calendar(int(command_args_opt[1])))
+            try:
+                print(calendar.calendar(int(command_args_opt[1])))
+            except:
+                print(
+                    "pysh: calendar: invalid operand. \n Try 'manual calendar' for more help.")
         else:
-            print("pysh: incorrect usage: try 'calendar year' or 'calendar year month'")
+            print(
+                "pysh: incorrect usage: try 'calendar [YEAR]' or 'calendar [YEAR] [MONTH]'")
     elif main_command == 'calc':
-        pass
-    elif main_command == 'who':
+        if len(command_args_opt) == 2:
+            try:
+                print(eval(command_args_opt[1]))
+            except:
+                print(
+                    "pysh: calc: invalid expression '{}'".format(command_args_opt[1]))
+        else:
+            print("pysh: incorrect usage: try 'calc [EXPR]'")
+    elif main_command == 'whoami':
         print(getpass.getuser())
     elif main_command == 'echo':
-        pass
+        print(' '.join(text for text in command_args_opt[1:]))
     elif main_command == 'rm':
+        if len(command_args_opt) == 2 or len(command_args_opt) == 3:
+            args_index = -1
+            dir_index = 1
+            if '-r' in command_args_opt:
+                args_index = command_args_opt.index('-r')
+                dir_index = 2 if (args_index == 1) else 1
+            if os.path.isfile(command_args_opt[1]):
+                os.remove(command_args_opt[dir_index])
+            elif os.path.isdir(command_args_opt[dir_index]):
+                if args_index == -1:
+                    try:
+                        os.rmdir(command_args_opt[dir_index])
+                    except:
+                        print("pysh: rm: cannot remove '{}': directory not empty".format(
+                            command_args_opt[dir_index]))
+                else:
+                    # try:
+                    shutil.rmtree(command_args_opt[dir_index])
+                    # except:
+                    #     print("pysh: rm: cannot remove '{}': No such file or directory".format(
+                    #         command_args_opt[dir_index]))
+            else:
+                print("pysh: rm: cannot remove '{}': No such file or directory".format(
+                    command_args_opt[dir_index]))
+        else:
+            print("pysh: incorrect usage: try 'rm [DIRECTORY]' or 'rm [FILE]'")
+    elif main_command == 'cat':
+        pass
+    elif main_command == 'cp':
+        pass
+    elif main_command == 'mv':
         pass
 
 
