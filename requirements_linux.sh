b8172565cfe118f591f218773ab20ef4c2f9555c
danascape <danascape@gmail.com>
2022-09-22 17:16:20 -0600

    pysh: linux: Check if python3 is installed
    * This is to make sure we have python3 installed on our environment
      before installing the python modules
    
    Signed-off-by: danascape <danascape@gmail.com>

--- requirements_linux.sh
+++ requirements_linux.sh
@@ -1,4 +1,9 @@
-pip3 install python-magic
-pip3 install psutil
-pip3 install git+https://github.com/rthalley/dnspython
-pip3 install pyinstaller
+if [[ -z "$(which python3)" ]]; then
+    echo "Install python3!"
+    exit 1
+else
+    pip3 install python-magic
+    pip3 install psutil
+    pip3 install git+https://github.com/rthalley/dnspython
+    pip3 install pyinstaller
+fi
